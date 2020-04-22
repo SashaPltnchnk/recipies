2b37901fe23dcd9f45f5231c36ac8a2a663c72d3
Zi <ZDubinaQ@gmail.com>
2020-04-22 22:12:11 +0300

    feat(app:recipe-list): create layout for recipe item, display list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipy',
+      'this is simply a test',
+      'https://media.voltron.voanews.com/Drupal/01live-166/styles/sourced/s3/2019-04/CF6AC768-3D66-4311-AF16-38BE8A4A7528.jpg?itok=7oLp0Gt-'),
+    ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
